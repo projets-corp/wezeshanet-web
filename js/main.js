2f90b3624a55a1f1457f4b643e1f2973fccf2431
Abe <43585616+AbeNalee@users.noreply.github.com>
2022-10-21 15:44:47 +0300

    content

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
     $(window).scroll(function(){
         
       var top = $(window).scrollTop();
-       if(top>=100){
-         $('.navbar').css('background','linear-gradient(135deg, rgba(231, 61, 89, 1) 0%, rgba(96, 74, 230, 1) 100%)'); 
-       }
-       
-       else 
-           if($('.navbar').css('background','linear-gradient(135deg, rgba(22, 37, 43, 1) 0%, rgba(22, 37, 43, 1) 100%)')){
+      if(top>=100){
+        $('.navbar').css('background','linear-gradient(135deg, rgba(59, 36, 114, 1) 0%, rgba(113, 64, 145, 1) 100%)'); 
+      }
+      
+      else 
+          if($('.navbar').css('background','linear-gradient(135deg, rgba(22, 37, 43, 1) 0%, rgba(22, 37, 43, 1) 100%)')){
 
-               $('.navbar').css('background','linear-gradient(135deg, rgba(44, 51, 56, 0) 0%, rgba(44, 51, 56, 0) 100%)'); 
-           }
+              $('.navbar').css('background','linear-gradient(135deg, rgba(44, 51, 56, 0) 0%, rgba(44, 51, 56, 0) 100%)'); 
+          }
    });
 
 
